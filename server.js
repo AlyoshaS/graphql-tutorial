70de70e014c35a44284a0246e17a6f61d28523ab
alyoshas <evelynschmitz1@gmail.com>
2019-01-09 01:03:55 -0200

    WIP

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
+// import express from 'express'
+// import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+
+// const server = express()
+
+// server.use('/graphiql', graphiqlExpress({
+//   endpointURL: "/graphql"
+// }))
+
+// server.listen(4000, () => {
+//     console.log('> listening on port 4000')
+// })
 import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
 
-const server = express()
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`
 
-server.get('/', (req, res) => {
-    res.send('<html><head><h1>Helloooo World!!</h1></head><body></body></html>')
-})
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+}
 
-server.listen(4000, () => {
-    console.log('> listening on port 4000')
-})
+const server = new ApolloServer({ typeDefs, resolvers })
+
+const app = express()
+server.applyMiddleware({ app })
+
+app.listen({ port: 4000 }, () =>
+  console.log(`> Server ready at http://localhost:4000${server.graphqlPath}`),
+)
